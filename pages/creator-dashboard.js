d570fcd5083b0042445d609ba1297f2030915f1c
AMIT KUMAR MISHRA <71893015+Amit0617@users.noreply.github.com>
2021-12-15 22:59:09 +0530

    Update creator-dashboard.js

--- pages/creator-dashboard.js
+++ pages/creator-dashboard.js
@@ -61,7 +61,7 @@ export default function CreatorDashboard() {
               <div key={i} className="border shadow rounded-xl overflow-hidden">
                 <img alt="" src={nft.image} className="rounded" />
                 <div className="p-4 bg-black">
-                  <p className="text-2xl font-bold text-white">Price - {nft.price} Eth</p>
+                  <p className="text-2xl font-bold text-white">Price - {nft.price} MATIC</p>
                 </div>
               </div>
             ))
